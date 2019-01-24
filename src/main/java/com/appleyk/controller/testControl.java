7a695f0162664bea8325fe0edf06d141ce47ab89
cafe3165 <78599297@qq.com>
2019-01-24 21:17:06 +0800

    根据配置生成地理位置

--- src/main/java/com/appleyk/controller/testControl.java
+++ src/main/java/com/appleyk/controller/testControl.java
@@ -1,7 +1,9 @@
 package com.appleyk.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.io.File;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import com.appleyk.relation.Provide;
 import com.appleyk.repository.DeviceRepository;
 //import com.appleyk.repository.LikeRelationRepository;
 import com.appleyk.repository.LocationRepository;
+import com.ConfigGenerate.LocationReader;
 import com.appleyk.RelationRepository.ProvideRepository;
 import com.appleyk.repository.ServiceRepository;
 import com.appleyk.repository.UserRepository;
@@ -34,47 +37,12 @@ public class testControl {
 	ProvideRepository provideRepository;
 //	@Autowired
 //	LikeRelationRepository likeRepository;
-	
-	
+
 	@RequestMapping("/test3")
-	 public String index() { 
-     return "Hello World 888";
-}
-//	@RequestMapping("/cuu")
-//	public void creatUser() {
-//		List<User> userNodes = new ArrayList<>();
-//		User userNode1 = new User();
-//		userNode1.setUid(1088L);
-//		System.out.println(userNode1.getUid());
-//		userNode1.setName("cafe");
-//		System.out.println(userNode1.getName());
-//		userNode1.setAge(22);
-//		userNode1.setSex("boy");
-//		userNode1.addHobby("play");
-//		userNode1.addHobby("sleep");
-////		
-//		User userNode2 = new User();
-//		userNode2.setUid(1077L);
-//		userNode2.setName("more");
-//		userNode2.setAge(17);
-//		userNode2.setSex("girl");
-//		userNode2.addHobby("eat");
-//		userNode2.addHobby("see");
-//		userNode2.addHobby("say");
-//		
-//		userNodes.add(userNode1);
-//		userNodes.add(userNode2);			
-//		Iterable<User> iterable = userRepository.save(userNodes);	
-////		User user=userRepository.save(userNode1);
-//		for (User user : iterable) {
-//			System.out.println("创建节点：【"+user.getName()+"】成功！");
-//		}
-//		
-//		
-//		
-//	}
-	
-	
+	public String index() {
+		return "Hello World 888";
+	}
+
 	@RequestMapping("/cd")
 	public void creatDevice() {
 		List<Device> devicesNodes = new ArrayList<>();
@@ -84,27 +52,24 @@ public class testControl {
 		dNode1.setLName("bedroom");
 		dNode1.setKey("Temperature");
 		dNode1.setValue(22);
-		
-		
+
 //		Device dNode2 = new Device();
 //		dNode2.setName("Light");
 //		dNode2.setDescribe("This is a light.");
 //		
 		devicesNodes.add(dNode1);
 //		devicesNodes.add(dNode2);			
-		Iterable<Device> iterable = deviceRepository.save(devicesNodes);	
+		Iterable<Device> iterable = deviceRepository.save(devicesNodes);
 ////		User user=userRepository.save(userNode1);
 		for (Device d : iterable) {
-			System.out.println("创建节点：【"+d.getDName()+"】成功！");
+			System.out.println("创建节点：【" + d.getDName() + "】成功！");
 		}
-		
-		
-		
+
 	}
-	
+
 	@RequestMapping("/cs")
 	public void createService() {
-		Service service=new Service(); 
+		Service service = new Service();
 		service.setName("S54");
 		service.setLName("bedroom");
 		service.setCType("Temperature");
@@ -113,34 +78,44 @@ public class testControl {
 		service.setState("on");
 		service.setValue(22);
 		serviceRepository.save(service);
-		
+
 	}
-	
-	
+
 	@RequestMapping("/cl")
 	public void createLocation() {
-		Location location=new Location(); 
-		location.setNN("L2");
-		location.setLName("bedroom");
-		locationRepository.save(location);
+		LocationReader dom4jParser = new LocationReader(new File("Location.xml"));
+		List<HashMap<String, String>> lMaps=dom4jParser.traversalDocumentByIterator();
+		String NN ="",LName = "";
+		List<Location> l = new ArrayList<>();
+		for (int i = 0; i < 3; i++) {
+			HashMap<String, String> map=lMaps.get(i);
+			NN=map.get("NN");
+			LName=map.get("LName");
+			Location location = new Location();
+			location.setNN(NN);
+			location.setLName(LName);
+			l.add(location);
+		}
+		locationRepository.save(l);
+
 	}
-	
+
 	@RequestMapping("/dps")
 	public void createDeviceProvideService() {
-			System.out.println("666");
-			Provide provide = new Provide();
-			Device startNode = new Device();
-			startNode.setDName("Airconditioner");
+		System.out.println("666");
+		Provide provide = new Provide();
+		Device startNode = new Device();
+		startNode.setDName("Airconditioner");
 //
-			Service endNode = new Service();
-			endNode.setDName("Airconditioner");
+		Service endNode = new Service();
+		endNode.setDName("Airconditioner");
 //
-			provide.setStartNode(startNode);
-			provide.setEndNode(endNode);
-			System.out.println(startNode.getDName());
-			System.out.println(endNode.getDName());
+		provide.setStartNode(startNode);
+		provide.setEndNode(endNode);
+		System.out.println(startNode.getDName());
+		System.out.println(endNode.getDName());
 //
-	List<Provide> provides = provideRepository.createLikes(startNode.getDName(),endNode.getDName());	
+		List<Provide> provides = provideRepository.createLikes(startNode.getDName(), endNode.getDName());
 //			
 //			/**
 //			 * 遍历创建的关系
@@ -151,7 +126,7 @@ public class testControl {
 //				System.out.println(d.getDName() + "--喜欢-->" + s.getName());
 //			}
 	}
-	
+
 //	@RequestMapping("/testlike")
 //	public void createLikeRelation() throws Exception {
 //
@@ -190,10 +165,5 @@ public class testControl {
 //	}
 //	
 //	
-	
-	
-	
+
 }
-
-
-	
